70a1ab2796fa068e5bb948b65db379829991dd52
Bel <belphe13@gmail.com>
2022-09-29 11:18:13 -0400

    map init

---
+++ index.js
@@ -0,0 +1,9 @@
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "557a0d92c92427a",
+      center: { lat: 48.85, lng: 2.35 },
+      zoom: 12,
+    });
+  }
+  
+window.initMap = initMap;
