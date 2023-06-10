fe74755cd3a5b835386296d43e82fda659f82e79
Bel <belphe13@gmail.com>
2023-06-10 03:21:00 -0400

    iOS in dev

---
+++ Radius/Radius/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Radius
+//
+//  Created by Lilan Yang on 10/13/22.
+//
+/*
+ * Copyright 2020 Google Inc. All rights reserved.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+/*
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 42.255975, longitude: -73.37121, zoom: 12.0)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+       
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+
+        self.view.addSubview(mapView)
+
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 42.255975, longitude: -73.371215)
+        marker.title = "Great Barrington"
+        marker.snippet = "USA"
+        marker.map = mapView
+        
+        // Adds a circle
+        let circleCenter = CLLocationCoordinate2D(latitude: 42.255975, longitude: -73.371215)
+        let circle = GMSCircle(position: circleCenter, radius: 1000)
+        circle.map = mapView
+  }
+}
+*/
+  
